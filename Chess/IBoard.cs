12e54a7bb20435effabea15064a2617ee6c694c7
DanielBhatti <bhattid@live.com>
2020-02-25 23:11:00 -0500

    Making interface classes.

---
+++ Chess/IBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public interface IBoard
+    {
+        void ResetBoard();
+
+        void UndoMove();
+
+        void SaveBoard();
+
+        void LoadBoard();
+
+        bool IsCheckState();
+
+        bool IsCheckMateState();
+
+        int NumberOfWhitePieces();
+
+        int NumberOfBlackPieces();
+
+        int NumberOfTiles();
+    }
+}
