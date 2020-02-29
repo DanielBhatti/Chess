dcc2a5586785e4dfbbc086cde0baacae9e60788c
DanielBhatti <bhattid@live.com>
2020-02-29 18:57:26 -0500

    Made a generic Piece class.

---
+++ Chess/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public class Piece : IPiece
+    {
+        public string Id { get; }
+        public Color Color { get; }
+        public Enum Status { get; }
+
+        public Piece(string id, Color color, Enum status)
+        {
+            Id = id;
+            Color = color;
+            Status = status;
+        }
+    }
+}
