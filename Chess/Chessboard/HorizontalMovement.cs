8bc532d2f5a1574479feeb491308c139d484c84b
DanielBhatti <bhattid@live.com>
2020-02-29 19:59:09 -0500

    Added HorizontalMovement class.

---
+++ Chess/Chessboard/HorizontalMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess.Chessboard
+{
+    public class HorizontalMovement : IMovement
+    {
+        public List<Vertex> GetConnectedVertices(Graph graph, Vertex vertex)
+        {
+            List<Vertex> connectedVertices = new List<Vertex>() { };
+            List<Edge> edges = graph.GetEdgesContainingVertex(vertex);
+            foreach(Edge edge in edges)
+            {
+                if (edge.Movement is HorizontalMovement)
+                {
+                    if (!edge.BeginningVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                    else if (!edge.EndingVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                }
+            }
+            return connectedVertices;
+        }
+    }
+}
