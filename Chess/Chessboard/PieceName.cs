b0ee7764ec192345e3b4eb73f81514c9ea2f4320
DanielBhatti <bhattid@live.com>
2020-02-29 19:10:51 -0500

    Enum containing the typical chess piece names.

---
+++ Chess/Chessboard/PieceName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Chessboard
+{
+    public enum PieceName
+    {
+        Piece,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King
+    }
+}
