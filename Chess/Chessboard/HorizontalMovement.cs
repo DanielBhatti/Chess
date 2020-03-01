561be01ca589e5fa5458b5dd9a23db01a8d1dbc1
DanielBhatti <bhattid@live.com>
2020-02-29 22:50:34 -0500

    Refactored a bit.

--- Chess/Chessboard/HorizontalMovement.cs
+++ Chess/Chessboard/HorizontalMovement.cs
@@ -5,21 +5,8 @@ using System.Text;
 
 namespace Chess.Chessboard
 {
-    public class HorizontalMovement : IMovement
+    public class HorizontalMovement : Movement
     {
-        public List<Vertex> GetConnectedVertices(Graph graph, Vertex vertex)
-        {
-            List<Vertex> connectedVertices = new List<Vertex>() { };
-            List<Edge> edges = graph.GetEdgesContainingVertex(vertex);
-            foreach(Edge edge in edges)
-            {
-                if (edge.Movement is HorizontalMovement)
-                {
-                    if (!edge.BeginningVertex.Equals(vertex)) connectedVertices.Add(vertex);
-                    else if (!edge.EndingVertex.Equals(vertex)) connectedVertices.Add(vertex);
-                }
-            }
-            return connectedVertices;
-        }
+
     }
 }
--- Chess/Movement.cs
+++ Chess/Movement.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace Chess
 {
-    class Movement
+    public class Movement : IMovement
     {
+        public List<Vertex> GetConnectedVertices(Graph graph, Vertex vertex)
+        {
+            List<Vertex> connectedVertices = new List<Vertex>() { };
+            List<Edge> edges = graph.GetEdgesContainingVertex(vertex);
+            foreach (Edge edge in edges)
+            {
+                if (edge.Movement.GetType() == this.GetType())
+                {
+                    if (!edge.BeginningVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                    else if (!edge.EndingVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                }
+            }
+            return connectedVertices;
+        }
+
+        public List<Vertex> GetBeginningVertices(Graph graph, Vertex vertex)
+        {
+            List<Vertex> connectedVertices = new List<Vertex>() { };
+            List<Edge> edges = graph.GetEdgesContainingVertex(vertex);
+            foreach (Edge edge in edges)
+            {
+                if (edge.Movement.GetType() == this.GetType())
+                {
+                    if (!edge.BeginningVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                    else if (!edge.EndingVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                }
+            }
+            return connectedVertices;
+        }
+
+        public List<Vertex> GetEndingVertices(Graph graph, Vertex vertex)
+        {
+            List<Vertex> connectedVertices = new List<Vertex>() { };
+            List<Edge> edges = graph.GetEdgesContainingVertex(vertex);
+            foreach (Edge edge in edges)
+            {
+                if (edge.Movement.GetType() == this.GetType())
+                {
+                    if (!edge.BeginningVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                    else if (!edge.EndingVertex.Equals(vertex)) connectedVertices.Add(vertex);
+                }
+            }
+            return connectedVertices;
+        }
     }
 }
