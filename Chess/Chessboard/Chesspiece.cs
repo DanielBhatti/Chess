2aaf1020008554f1ac0221f8ef9095269ab88292
DanielBhatti <bhattid@live.com>
2020-02-29 19:33:34 -0500

    Added Chesspiece class.  Exactly the same as the piece class, but ha a PieceName enum for all the pieces.

---
+++ Chess/Chessboard/Chesspiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Chessboard
+{
+    public class Chesspiece : Piece
+    {
+        public PieceName PieceName { get; private set; }
+
+        public Chesspiece(string id, Color color, PieceStatus status, PieceName pieceName) : base(id, color, status)
+        {
+            PieceName = pieceName;
+        }
+    }
+}
